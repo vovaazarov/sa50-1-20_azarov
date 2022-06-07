2ab078c10d12bc96a677e237b70d963af3c9128a
azarov <azarov@gmail.com>
2022-06-07 21:39:35 +0300

    ne rabotaet

--- pr15/script.js
+++ pr15/script.js
@@ -1,14 +1,27 @@
-function move() {
-    var elem = document.getElementsById("cube")
-    var pos = 0;
-    var id = setInterval(frame, 10)
-
-    function frame() {
-        while (pos < 350) {
-            pos++;
-            elem.style.top = pos + "px";
-            elem.style.left = pos + "px";
+function Animation(){
+    let elem = document.getElementById("block");
+    let pos = 0;
+    let top = 0;
+    let time = setInterval (frame, 2);
+    let speed_top = 0;
+    let speed_left = 1;
+    function frame(){
+        if(pos==0 && top==0){
+        speed_left = 1;
+        speed_top = 0;
+        }else if(pos==700 && top==0){
+        speed_left = 0;
+        speed_top = 1;
+        }else if(pos==700 && top==700){
+        speed_left = -1;
+        speed_top = 0;
+        }else if(pos==0 && top==700){
+        speed_left = 0;
+        speed_top = -1;
         }
-
+        top+= speed_top;
+        elem.style.top = top + "px";
+        pos+=speed_left;
+        elem.style.left = pos + "px";
+        }
     }
-}
